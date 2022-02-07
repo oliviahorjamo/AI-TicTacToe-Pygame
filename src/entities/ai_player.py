5ca04ebccc730f0d47cca97801c1402e2bdee3ac
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2022-02-07 20:07:03 +0200

    minor fix to minimax

--- src/entities/ai_player.py
+++ src/entities/ai_player.py
@@ -7,46 +7,22 @@ class AiPlayer:
 
     def minimax(self, node, depth, alpha, beta, maximizingPlayer):
 
-        """Pseudocode: https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning
-
-        function alphabeta(node, depth, α, β, maximizingPlayer) is
-            if depth = 0 or node is a terminal node then
-                return the heuristic value of node
-            if maximizingPlayer then
-                value := −∞
-                for each child of node do
-                    value := max(value, alphabeta(child, depth − 1, α, β, FALSE))
-                    α := max(α, value)
-                    if value ≥ β then
-                        break (* β cutoff *)
-                return value
-            else
-                value := +∞
-                for each child of node do
-                    value := min(value, alphabeta(child, depth − 1, α, β, TRUE))
-                    β := min(β, value)
-                    if value ≤ α then
-                        break (* α cutoff *)
-                return value
-
-            (* Initial call *)
-            alphabeta(origin, depth, −∞, +∞, TRUE)
-            """
-
-        if depth == 0 or self.game.check_for_tie(self.game_board.board):
+        if depth == 0 or self.game.check_for_win(self.game_board.board, self.game_board.board_size):
             return 0
 
         if maximizingPlayer:
             value = -math.inf
+
             for node in self.game_board.board:
                 if self.game.check_for_space(node[0], node[1], self.game_board.board):
-                    self.game.insert_move(1, node[0], node[1], self.game_board.board)
+                    self.game.insert_move(2, node[0], node[1], self.game_board.board)
                     value = max(value, self.minimax(node, depth - 1, alpha, beta, False))
                     self.game.insert_move(0, node[0], node[1], self.game_board.board)
                     alpha = max(alpha, value)
                     if value >= beta:
                         break
             return value
+
         else:
             value = math.inf
             for node in self.game_board.board:
@@ -54,9 +30,10 @@ class AiPlayer:
                     self.game.insert_move(1, node[0], node[1], self.game_board.board)
                     value = min(value, self.minimax(node, depth - 1, alpha, beta, True))
                     self.game.insert_move(0, node[0], node[1], self.game_board.board)
-                    beta = min(beta, value)
+                    alpha = max(alpha, value)
                     if value <= alpha:
                         break
+                    beta = min(beta, value)
             return value
 
     def find_best_move(self):
